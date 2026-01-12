cdb48f6f47f08c43212929292e39e1a2d01a0e76
Véro Grué <veronique.gru@educa.jcyl.com>
2026-01-12 07:18:29 +0100

    Add Cambios de las funciones de la tabla de puntuación para adecuarse a los niveles

--- SopaLetras/webroot/js/tablaPuntuacion.js
+++ SopaLetras/webroot/js/tablaPuntuacion.js
@@ -4,68 +4,38 @@ export { guardarTiempoJuego, mostrarPosicion,esTiempoTop3 };
 Funcion para guardar los tiempos de juego.
 ==================*/
 
-// function guardarTiempoJuego(nombre, tiempoJuego) {
-//     //se leen los tiempos que hay con localStorage. https://www.w3schools.com/jsref/prop_win_localstorage.asp
-//     // no se guarda en un json fisico, es un almacenamiento en el navegador.
-
-//     let tiempos = JSON.parse(localStorage.getItem("mejoresTiempos")) || [];
-//     //se añade el nuevo tiempo asociado  al nombre
-//     tiempos.push({ 
-//         nombre: nombre,
-//         tiempoJuego: tiempoJuego });
-//     //se ordena de menor a mayor    
-//     tiempos.sort((a, b) => a.tiempoJuego - b.tiempoJuego);
-//     //slice es para guardar solo los 3 primeros
-//     tiempos = tiempos.slice(0, 3); 
-//     //se gurada en localStorage. https://www.w3schools.com/js/js_json.asp
-//     localStorage.setItem("mejoresTiempos", JSON.stringify(tiempos));
-
-//     console.log("Tiempo guardado: ", {nombre, tiempoJuego});
-// }
 function guardarTiempoJuego(nombre, tiempoJuego) {
     //se leen los tiempos que hay con localStorage. https://www.w3schools.com/jsref/prop_win_localstorage.asp
     // no se guarda en un json fisico, es un almacenamiento en el navegador.
-    const oMejoresPuntuaciones = {
-  facil: {
-    primera: ["", 0],  
-    segunda: ["", 0],
-    tercera: ["", 0]
-  },
-  intermedio: {
-    primera: ["", 0],
-    segunda: ["", 0],
-    tercera: ["", 0]
-  },
-  dificil: {
-    primera: ["", 0],
-    segunda: ["", 0],
-    tercera: ["", 0]
-  }
-};
+    //Se lee el objeto con los niveles y si no existe se crea un objeto con los 3 niveles vacios.
+    let datosPuntuacion = JSON.parse(localStorage.getItem("mejoresTiempos")) || {facil:[], medio:[],dificil:[]};
 
-    let tiempos = JSON.parse(localStorage.getItem("mejoresTiempos")) || [];
-    //se añade el nuevo tiempo asociado  al nombre
-    tiempos.push({ 
+    //Se selecciona el array correspondiente al nivel actual
+    let tiemposNivel = datosPuntuacion[nivel];
+
+    //se añade el nuevo tiempo asociado  al nombre y toamndo en cuenta el nivel
+    tiemposNivel.push({ 
         nombre: nombre,
         tiempoJuego: tiempoJuego });
-    //se ordena de menor a mayor    
-    tiempos.sort((a, b) => a.tiempoJuego - b.tiempoJuego);
-    //slice es para guardar solo los 3 primeros
-    tiempos = tiempos.slice(0, 3); 
-    //se gurada en localStorage. https://www.w3schools.com/js/js_json.asp
-    localStorage.setItem("mejoresTiempos", JSON.stringify(tiempos));
 
-    console.log("Tiempo guardado: ", {nombre, tiempoJuego});
+    //se ordena de menor a mayor y solo se coge 3 mejores tiempos
+    tiemposNivel.sort((a, b) => a.tiempoJuego - b.tiempoJuego);
+    //slice es para guardar solo los 3 primeros
+    datosPuntuacion[nivel] = tiemposNivel.slice(0, 3); 
+    //se gurada en localStorage. https://www.w3schools.com/js/js_json.asp
+    localStorage.setItem("mejoresTiempos", JSON.stringify(datosPuntuacion));
+
+    console.log("Tiempo guardado en ${nivel}: ", {nombre, tiempoJuego});
 }
 
-function esTiempoTop3(tiempoJuego) {
-    let tiempos = JSON.parse(localStorage.getItem("mejoresTiempos")) || [];
+function esTiempoTop3(tiempoJuego, nivel) {
+    let datosPuntuacion = JSON.parse(localStorage.getItem("mejoresTiempos")) || {facil:[], medio:[],dificil:[]};
     //si hay meno de 3 tiempos entonces esta en el top3
-    if(tiempos.length<3){
+    if(datosPuntuacion[nivel].length<3){
         return true;
     }
     //se comparan los tiempos con el peor tiempo para ver si es mejor o no. saca el valor del 3er puesto
-    let peorTiempo=tiempos[tiempos.length-1].tiempoJuego;
+    let peorTiempo=datosPuntuacion[nivel][datosPuntuacion[nivel].length-1].tiempoJuego;
     //se rotorna true o false si cumple o no la condicion
     return tiempoJuego<peorTiempo;
 }
@@ -75,115 +45,18 @@ function esTiempoTop3(tiempoJuego) {
 Funcion para mostrar la tabla con los mejores tiempos
 ==================*/
 
-// function mostrarPosicion() {
+function mostrarPosicion(nivel) {
 
-//    let tiempos = JSON.parse(localStorage.getItem("mejoresTiempos")) || [];
-//      console.log(" MostrarPosicion: tabla con los datos:", tiempos);
-//     // Si hay menos de 3 resultados, se completa con vacíos
-//     while (tiempos.length < 3) {
-//         tiempos.push({ nombre: "-", tiempoJuego: "-" });
-//     }
-//     // Crear cabecera de la tabla
-//     const tabla = document.createElement("table");
-//     const filaCabecera = document.createElement("tr");
-//     const thPosicion = document.createElement("th");
-//     thPosicion.textContent = "Posición";
-
-//     const thJugador = document.createElement("th");
-//     thJugador.textContent = "Jugador";
-
-//     const thTiempo = document.createElement("th");
-//     thTiempo.textContent = "Tiempo";
-
-//     filaCabecera.appendChild(thPosicion);
-//     filaCabecera.appendChild(thJugador);
-//     filaCabecera.appendChild(thTiempo);
-
-//     tabla.appendChild(filaCabecera);
-
-//     // Crear filas de datos
-//     const top3 = tiempos.slice(0, 3);//solo los tres mejores timpos. Solo 3 filas
-
-//     for (let i = 0; i < top3.length; i++) {
-
-//         const fila = document.createElement("tr");
-
-//         const celdaPos = document.createElement("td");
-//         celdaPos.textContent = i + 1;
-
-//         const celdaJug = document.createElement("td");
-//         celdaJug.textContent = top3[i].nombre;
-//         // //crear un input dentro de la celda pra que el usuario pueda escribir su nombre
-//         // const inputNombre = document.createElement("input");
-//         // //indicar el tipo del input
-//         // inputNombre.type = "text";
-//         // inputNombre.value= top3[i].nombre;
-//         // //añadir el input a la celda
-//         // celdaJug.appendChild(inputNombre); 
-
-//         const celdaTiempo = document.createElement("td");
-//         celdaTiempo.textContent = top3[i].tiempoJuego;
-
-//         fila.appendChild(celdaPos);
-//         fila.appendChild(celdaJug);
-//         fila.appendChild(celdaTiempo);
-
-//         tabla.appendChild(fila);
-//     }
-
-//     const contenedor = document.getElementById("tablaPosicion");
-//     //hay que limpiar el contendor porque sino sale varias veces la tabla
-//      contenedor.innerHTML = ""; 
-//     contenedor.appendChild(tabla);
-//     //console.log(" Contenedor mostrarPosicion:", contenedor);
-// }
-
-
-const oMejoresPuntuaciones = {
-  facil: {
-    primera: ["", 0],  
-    segunda: ["", 0],
-    tercera: ["", 0]
-  },
-  intermedio: {
-    primera: ["", 0],
-    segunda: ["", 0],
-    tercera: ["", 0]
-  },
-  dificil: {
-    primera: ["", 0],
-    segunda: ["", 0],
-    tercera: ["", 0]
-  }
-};
-
-// oMejoresPuntuaciones,nivel=0
-function mostrarPosicion(nivel=0) {
-const oMejoresPuntuaciones = {
-  facil: {
-    primera: ["d", 2],  
-    segunda: ["f", 6],
-    tercera: ["g", 9]
-  },
-  intermedio: {
-    primera: ["", 1],
-    segunda: ["", 2],
-    tercera: ["", 3]
-  },
-  dificil: {
-    primera: ["", 4],
-    segunda: ["", 5],
-    tercera: ["", 7]
-  }
-};
-    
-
-   let tiempos = JSON.parse(localStorage.getItem(oMejoresPuntuaciones.nivel)) || []
-     console.log(" MostrarPosicion: tabla con los datos:", tiempos);
+    //Se lee el objeto con los niveles y si no existe se crea un objeto con los 3 niveles vacios.
+    let datosPuntuacion = JSON.parse(localStorage.getItem("mejoresTiempos")) || {facil:[], medio:[],dificil:[]};
+     console.log(" MostrarPosicion: tabla con los datos:", datosPuntuacion[nivel]);
     // Si hay menos de 3 resultados, se completa con vacíos
 
-    while (tiempos.length < 3) {
-        tiempos.push({ nombre: "-", tiempoJuego: "-" });
+    //Se selecciona el array correspondiente al nivel actual. Cogemos solo los tiempo del nivel
+    let tiemposNivel = datosPuntuacion[nivel];
+  console.log(" MostrarPosicion: tabla con los datos del nivel:", tiemposNivel);
+    while (tiemposNivel.length < 3) {
+        tiemposNivel.push({ nombre: "-", tiempoJuego: "-" });
     }
     // Crear cabecera de la tabla
     const tabla = document.createElement("table");
@@ -204,7 +77,7 @@ const oMejoresPuntuaciones = {
     tabla.appendChild(filaCabecera);
 
     // Crear filas de datos
-    const top3 = tiempos.slice(0, 3);//solo los tres mejores teimpos. Solo 3 filas
+    const top3 = tiemposNivel.slice(0, 3);//solo los tres mejores timpos. Solo 3 filas
 
     for (let i = 0; i < top3.length; i++) {
 
@@ -215,7 +88,13 @@ const oMejoresPuntuaciones = {
 
         const celdaJug = document.createElement("td");
         celdaJug.textContent = top3[i].nombre;
-       
+        // //crear un input dentro de la celda pra que el usuario pueda escribir su nombre
+        // const inputNombre = document.createElement("input");
+        // //indicar el tipo del input
+        // inputNombre.type = "text";
+        // inputNombre.value= top3[i].nombre;
+        // //añadir el input a la celda
+        // celdaJug.appendChild(inputNombre); 
 
         const celdaTiempo = document.createElement("td");
         celdaTiempo.textContent = top3[i].tiempoJuego;
@@ -228,11 +107,21 @@ const oMejoresPuntuaciones = {
     }
 
     const contenedor = document.getElementById("tablaPosicion");
-    //hay que limpiar el contendor porque sino sale varias veces la tabla
-     contenedor.innerHTML = ""; 
-    contenedor.appendChild(tabla);
+    if(contenedor){
+      //hay que limpiar el contendor porque sino sale varias veces la tabla
+     contenedor.innerHTML = "";
+     //se pone un titulo a la tabla para saber el nivel
+     const tituloTabla = document.createElement("h3");
+     tituloTabla.textContent='Mejores Tiempos - Nivel'+ nivel;
+      contenedor.appendChild(tituloTabla);
+      contenedor.appendChild(tabla);
     //console.log(" Contenedor mostrarPosicion:", contenedor);
+    }
+     
+    
 }
+
+
 
 
 
